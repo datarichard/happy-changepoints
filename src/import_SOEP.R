f93d4cf0cd9cb26dfc3d349663c7e1c0978078d8
Richard Morris <28824961+datarichard@users.noreply.github.com>
2023-11-21 11:16:01 +1100

    Update import_SOEP.R

--- src/import_SOEP.R
+++ src/import_SOEP.R
@@ -1,1 +1,19 @@
 # Import SOEP data from source
+
+#### Setup ####
+library(tidyverse)
+library(haven)
+
+#### Import files ####
+SOEP_files <- list.files("../data/SOEP/SPSS_EN")
+
+# import files to environment from SOEP_files
+
+#### Preprocessing ####
+
+
+#### Join data frame ####
+# dataframe_to_save <- left_join()
+
+#### Save results ####
+write_rds(dataframe_to_save, "../results/preprocessed_SOEP.rds")
