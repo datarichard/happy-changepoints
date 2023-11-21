d1d3143dd3f57d3d9e413e4b5443c8307190c0b2
Richard Morris <28824961+datarichard@users.noreply.github.com>
2023-11-21 11:27:16 +1100

    Update import_SOEP.R
    added some code structure and comments

--- src/import_SOEP.R
+++ src/import_SOEP.R
@@ -7,13 +7,19 @@ library(haven)
 #### Import files ####
 SOEP_files <- list.files("../data/SOEP/SPSS_EN")
 
-# import files to environment from SOEP_files
+# remember to deal with SPSS labels correctly
+SOEP_health <- read_spss(SOEP_files[1])
+SOEP_pequiv <- read_spss(SOEP_files[1])
 
-#### Preprocessing ####
+                      
+#### Select variables I want ####
+mcs <- select(SOEP_health, mcs)
+hh_income <- select(SOEP_pequiv, income)
 
 
-#### Join data frame ####
-# dataframe_to_save <- left_join()
+
+#### Join the variables I want into a data frame ####
+# dataframe_to_save <- left_join(mcs, hh_income, by = "xwaveid")
 
 #### Save results ####
-write_rds(dataframe_to_save, "../results/preprocessed_SOEP.rds")
+write_rds(dataframe_to_save, "../results/imported_SOEP.rds")
