24651609b6536b3169a435b391d776db7bfcd478
Richard Morris <28824961+datarichard@users.noreply.github.com>
2023-11-21 11:07:29 +1100

    added run file

---
+++ run.R
@@ -0,0 +1,18 @@
+# Run file created by RMorris 
+
+#### Import ####
+source("src/import_SOEP.R")
+
+
+
+#### Preprocessing ####
+source("src/preprocess_SOEP_MCS.R")
+
+
+
+#### Model fitting ####
+source("src/fit_SOEP_MCS.R")
+
+
+#### Plot changepoints ####
+source("scr/plot_SOEP_MCS_CPs.R")
