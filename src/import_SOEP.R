076bf2e912d9528baf26dfe24f65ce37aed28f2b
sam-prince2 <93499080+sam-prince2@users.noreply.github.com>
2023-11-23 09:52:45 +1100

    Updated import_SOEP.R

--- src/import_SOEP.R
+++ src/import_SOEP.R
@@ -1,29 +1,3 @@
-# Import SOEP data from source
-
-#### Setup ####
-library(tidyverse)
-library(haven)
-
-#### Import files ####
-SOEP_files <- list.files("../data/SOEP/SPSS_EN")
-
-# remember to deal with SPSS labels correctly
-SOEP_health <- read_spss(SOEP_files[1])
-SOEP_pequiv <- read_spss(SOEP_files[1])
-
-                      
-#### Select variables I want ####
-mcs <- select(SOEP_health, mcs)
-hh_income <- select(SOEP_pequiv, income)
-
-
-
-#### Join the variables I want into a data frame ####
-# dataframe_to_save <- left_join(mcs, hh_income, by = "xwaveid")
-
-#### Save results ####
-write_rds(dataframe_to_save, "../results/imported_SOEP.rds")
-
 ###SETUP ####
 library(tidyverse)
 library(haven)
